6ce9b8b635aef37fd3439833a7c21de3310bb4eb
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2022-08-12 16:13:00 +0300

    Add test endpoint

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from app import flask_app
+from app.rest.color_api import color_api_blueprint
+
+flask_app.register_blueprint(color_api_blueprint)
 
 if __name__ == '__main__':
     flask_app.run(debug=True)
