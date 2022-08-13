8c7fe52fd748d1fc39e0c7d4e31bc1a90989d506
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2022-08-13 13:27:35 +0300

    Add marshmallow module, create schemes and palette get endpoint prototype

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from config import user, password, database
 
 flask_app = Flask(__name__)
+engine = create_engine(f'mysql://{user}:{password}@127.0.0.1:3306/{database}')
--- app/rest/color_api.py
+++ app/rest/color_api.py
@@ -1,9 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app import engine
+from sqlalchemy.orm import Session
+from app.models.models import Palette
+from app.schemas.schemas import PaletteSchema
 
 
 color_api_blueprint = Blueprint('color_api_blueprint', __name__)
 
 
-@color_api_blueprint.route('/', methods=['GET'])
-def hello():
-    return "Hello!"
+@color_api_blueprint.route('/palette/<id>', methods=['GET'])
+def show_palette(id):
+    with Session(engine) as session:
+        palette = session.query(Palette).filter_by(id=id).one()
+        return jsonify(PaletteSchema().dump(palette))
